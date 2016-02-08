8df53072569cf3df2e40a03099f84d6f7f287224
cyotee <cyotee@gmail.com>
2016-02-08 02:39:53 -0800

    Updated to latest version

--- fuse_6.1.0/docker_init.sh
+++ fuse_6.1.0/docker_init.sh
@@ -3,7 +3,9 @@
 # Start Fuse
 echo "Starting fuse..."
 /opt/jboss/jboss-fuse-6.1.1.redhat-457/bin/start
-sleep 30
+sleep 10
+echo "Fuse Starting"
+sleep 20
 echo "Fuse started"
 
 tail -f /opt/jboss/jboss-fuse-6.1.1.redhat-457/data/log/fuse.log
